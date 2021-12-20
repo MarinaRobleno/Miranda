b79337b2a8131afcefb8cdbbc64962051fbf1c86
Marina Robleño <marinarsanisidro@gmail.com>
2021-12-20 16:42:26 +0100

    Geocoder added, not functional yet

--- js/map.js
+++ js/map.js
@@ -1,17 +1,24 @@
+let map;
+let marker;
+let geocoder;
+let responseDiv;
+let response;
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 6,
     center: { lat: 40.2085, lng: -3.713 },
+    mapTypeControl: false,
   });
   const infoWindow = new google.maps.InfoWindow({
     content: "",
     disableAutoPan: true,
   });
+
   // Create an array of alphabetical characters used to label the markers.
   // Add some markers to the map.
   const svgMarker = {
     path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-    fillColor: "#bead8e",
+    fillColor: "#000000",
     fillOpacity: 1,
     strokeWeight: 0,
     rotation: 0,
@@ -49,7 +56,7 @@ function initMap() {
         });
 
         communityPolygon.setMap(map);
-        selected.name=place.community;
+        selected.name = place.community;
       }
     });
     return marker;
@@ -86,6 +93,77 @@ function initMap() {
       handleLocationError(false, infoWindow, map.getCenter());
     }
   });
+  geocoder = new google.maps.Geocoder();
+
+  const inputText = document.createElement("input");
+
+  inputText.type = "text";
+  inputText.placeholder = "Enter a location";
+
+  const submitButton = document.createElement("input");
+
+  submitButton.type = "button";
+  submitButton.value = "Geocode";
+  submitButton.classList.add("button", "button-primary");
+
+  const clearButton = document.createElement("input");
+
+  clearButton.type = "button";
+  clearButton.value = "Clear";
+  clearButton.classList.add("button", "button-secondary");
+  response = document.createElement("pre");
+  response.id = "response";
+  response.innerText = "";
+  responseDiv = document.createElement("div");
+  responseDiv.id = "response-container";
+  responseDiv.appendChild(response);
+
+  const instructionsElement = document.createElement("p");
+
+  instructionsElement.id = "instructions";
+  instructionsElement.innerHTML =
+    "<strong>Instructions</strong>: Enter an address in the textbox to geocode or click on the map to reverse geocode.";
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputText);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(submitButton);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(clearButton);
+  map.controls[google.maps.ControlPosition.LEFT_TOP].push(instructionsElement);
+  map.controls[google.maps.ControlPosition.LEFT_TOP].push(responseDiv);
+  marker = new google.maps.Marker({
+    map,
+  });
+  map.addListener("click", (e) => {
+    geocode({ location: e.latLng });
+  });
+  submitButton.addEventListener("click", () =>
+    geocode({ address: inputText.value })
+  );
+  clearButton.addEventListener("click", () => {
+    clear();
+  });
+  clear();
+}
+function clear() {
+  marker.setMap(null);
+  responseDiv.style.display = "none";
+}
+
+function geocode(request) {
+  clear();
+  geocoder
+    .geocode(request)
+    .then((result) => {
+      const { results } = result;
+
+      map.setCenter(results[0].geometry.location);
+      marker.setPosition(results[0].geometry.location);
+      marker.setMap(map);
+      responseDiv.style.display = "block";
+      response.innerText = JSON.stringify(result, null, 2);
+      return results;
+    })
+    .catch((e) => {
+      alert("Geocode was not successful for the following reason: " + e);
+    });
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
