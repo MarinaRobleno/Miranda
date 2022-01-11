8591870fda08bce15359e0babba533d7222c4717
Marina Robleño <marinarsanisidro@gmail.com>
2022-01-11 16:44:30 +0100

    Functional check in and out calendars

---
+++ js/flatpickr.js
@@ -0,0 +1,6 @@
+function execute_flatpickr() {
+    const config = {
+        dateFormat: "Y-m-d",
+    };
+    flatpickr(".check-in-input", config);
+}
--- js/main.js
+++ js/main.js
@@ -1,23 +1,29 @@
 const openButton = document.getElementById("hamburger-button");
 let dropdownContent = document.getElementById("drop-down-box");
 
-//Hamburger menu function
-function openDropdown() {
-  dropdownContent.classList.toggle("drop-down-box--visible");
-}
-openButton.addEventListener("click", openDropdown);
+document.addEventListener("DOMContentLoaded", function () {
+  //Hamburger menu function
+  function openDropdown() {
+    dropdownContent.classList.toggle("drop-down-box--visible");
+  }
+  openButton.addEventListener("click", openDropdown);
 
-$(".arrow-down-section").click(function () {
-  var $target = $(".scroll-section.active").next(".scroll-section");
-  if ($target.length == 0) $target = $(".scroll-section:first");
+  //Arrow down
+  $(".arrow-down-section").click(function () {
+    var $target = $(".scroll-section.active").next(".scroll-section");
+    if ($target.length == 0) $target = $(".scroll-section:first");
 
-  $("html, body").animate(
-    {
-      scrollTop: $target.offset().top,
-    },
-    "slow"
-  );
+    $("html, body").animate(
+      {
+        scrollTop: $target.offset().top,
+      },
+      "slow"
+    );
 
-  $(".active").removeClass("active");
-  $target.addClass("active");
+    $(".active").removeClass("active");
+    $target.addClass("active");
+  });
+
+  //Flatpickr
+  execute_flatpickr();
 });
