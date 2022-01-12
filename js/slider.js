efdcf9ce8cc84b5accfb16a0ef639399342c646e
Marina Robleño <marinarsanisidro@gmail.com>
2022-01-12 11:55:05 +0100

    Testing image carousell

--- js/slider.js
+++ js/slider.js
@@ -3,6 +3,8 @@ function execute_slider() {
         type    : 'loop',
         perPage : 3,
         autoplay: true,
+        perMove: 1,
+        gap: 10,
       } );
       
       splide.mount();
