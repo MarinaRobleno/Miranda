c17bac4fb054d478c3a43e295ac15a3c085c3a5e
Marina Robleño <marinarsanisidro@gmail.com>
2022-01-13 15:37:41 +0100

    Changed background color, added low opacity images

--- js/slider.js
+++ js/slider.js
@@ -4,12 +4,12 @@ const roomType1 = [
   "Images/pexels-quark-studio-2507014.jpg",
   "https://media-cdn.tripadvisor.com/media/photo-s/0e/cc/fa/fc/habitacion-con-jacuzzi.jpg",
   "https://media-cdn.tripadvisor.com/media/photo-s/13/d8/ea/1b/a-room-at-the-beach.jpg",
-  "https://www.hotelfariones.es/backoffice/images/1402-df7b4d7a-e54c-45df-8878-544f85a35d79.jpg"
-]
+  "https://www.hotelfariones.es/backoffice/images/1402-df7b4d7a-e54c-45df-8878-544f85a35d79.jpg",
+];
 
 const splider_array = document.getElementById("splide__list");
 
-for (let i = 0; i<roomType1.length; i++){
+for (let i = 0; i < roomType1.length; i++) {
   splider_container = document.createElement("li");
   splider_container.classList.add("splide__slide");
   splider_image = document.createElement("img");
@@ -18,17 +18,19 @@ for (let i = 0; i<roomType1.length; i++){
   splider_container.appendChild(splider_image);
   splider_array.appendChild(splider_container);
   image_counter = document.getElementById("carousel-detail-box-counter");
-  image_counter.innerText = `${i+1}/${roomType1.length}`;
+  //image_counter.innerText = `${i+1}/${roomType1.length}`;
 }
 
 function execute_slider() {
-    var splide = new Splide( '.splide', {
-        type    : 'loop',
-        perPage : 3,
-        autoplay: true,
-        perMove: 1,
-        gap: 10,
-      } );
-      
-      splide.mount();
+  var splide = new Splide(".splide", {
+    type: "loop",
+    perPage: 4,
+    autoplay: true,
+    perMove: 1,
+    rewindSpeed: 8000,
+    focus: "center",
+    speed: 400,
+  });
+
+  splide.mount();
 }
